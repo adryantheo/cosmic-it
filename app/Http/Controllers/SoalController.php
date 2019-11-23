1401703c932d5e1390b359c8f1246c5b75afa178
Alfha Omega <toviddd@gmail.com>
2019-11-23 11:35:53 +0700

    soal controller

--- app/Http/Controllers/SoalController.php
+++ app/Http/Controllers/SoalController.php
@@ -7,33 +7,17 @@ use Illuminate\Http\Request;
 
 class SoalController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
         return response()->json(Soal::all(), 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
@@ -50,47 +34,22 @@ class SoalController extends Controller
             return response()->json('gagal simpan', 400);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Soal  $soal
-     * @return \Illuminate\Http\Response
-     */
     public function show(Soal $soal)
     {
         //
         return response()->json($soal, 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Soal  $soal
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Soal $soal)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Soal  $soal
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Soal $soal)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Soal  $soal
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Soal $soal)
     {
         //

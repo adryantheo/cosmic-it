152022b8f0349207b9758eeac3e2b6bbe7bbc40e
Alfha Omega <toviddd@gmail.com>
2019-11-23 10:17:24 +0700

    UserController (fungsi login)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,25 +9,41 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function login(Request $requests){
+    // public function login(Request $requests){
         
-        $user = new User();
+    //     $user = new User();
 
-        $status = $user::create([
-            $requests->email,
-            $requests->password,
-        ]);
-        return response()->json([
-            'Status' => $status, 
-        ]);
+    //     $status = $user::create([
+    //         $requests->email,
+    //         $requests->password,
+    //     ]);
+    //     return response()->json([
+    //         'Status' => $status, 
+    //     ]);
+    // }
+    public function login(Request $request){
+        $status= 401;
+        $response= ['error' => 'Unauthorised'];
+        if (Auth::attempt($request->only(['email','password']))) {
+            $status= 200;
+            $response= [
+                'user' => Auth::user(),
+                'token' => Auth::user()->createToken('token')->accessToken,
+            ];
+        }
+        return response()->json($response, $status);
     }
+
     public function register(){
 
     }
+
     public function getUser(User $user){
 
     }
     public function updateScore(Request $requests, User $user){
+        $status= $user->update();
 
     }
+
 }

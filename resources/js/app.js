3fd98f7654bbacb8339e458668fd345d895382b8
Alfha Omega <toviddd@gmail.com>
2019-11-23 13:24:22 +0700

    change app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,10 @@
+window.vue= require('vue');
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import VueRouter from 'vue-router'
 import router from './router.js'
-
 Vue.use(router);
-window.vue= require('vue)';
+
 window.EventBus= ;
 window.axios= create.axios();
 
@@ -18,5 +18,6 @@ Vue.prototype.$user= User;
 const vue = new Vue({
     el: '#app',
     router,
+    VueRouter,
     component: './AppRoot'
 })
